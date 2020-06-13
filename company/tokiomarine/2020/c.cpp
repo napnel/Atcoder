040085ef726c9c19ac85164961736cef1a6509e0
Shimejime <yoshi3njm@gmail.com>
2020-06-13 23:52:41 +0900

    solved tokiomarine/c.cpp

--- company/tokiomarine/2020/c.cpp
+++ company/tokiomarine/2020/c.cpp
@@ -23,21 +23,53 @@ template<typename T>
 ostream &operator<<(ostream &os, const vector<T> &v) { for(int i = 0; i < (int) v.size(); i++) os << v[i] << (i + 1 != v.size() ? " " : ""); return os; }
 
 int n, k;
-vector<set<int>> G;
 void solve()
 {
     cin >> n >> k;
     vector<int> A(n);
-    G.resize(n);
     for(int i = 0; i < n; i++) cin >> A[i];
 
-    for(int i = 0; i < n; i++)
+    while(k--)
     {
-        int left = i - A[i];
-        int right = i + A[i];
-        G[left].insert(i);
-        G[right].insert(i);
+        bool is_convergence = true;
+        // 左右からimos法を行う
+        vector<int> imos1(n, 0), imos2(n, 0);
+        for (int i = 0; i < n; i++)
+        {
+            imos1[i]++;
+            if(i + A[i] + 1 < n)
+            {
+                imos1[i + A[i] + 1]--;
+            }
+        }
+        for(int i = 1; i < n; i++)
+        {
+            imos1[i] += imos1[i - 1];
+        }
+
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(A[i] == 0 || i - 1 < 0) continue;
+            imos2[i - 1]++;
+            if(i - A[i] - 1 >= 0)
+            {
+                imos2[i - A[i] - 1]--;
+            }
+        }
+        for(int i = n - 2; i >= 0; i--)
+        {
+            imos2[i] += imos2[i + 1];
+        }
+        vector<int> B(n);
+        for(int i = 0; i < n; i++)
+        {
+            B[i] = imos1[i] + imos2[i];
+            if(B[i] != n) is_convergence = false;
+        }
+        A = B;
+        if(is_convergence) break;
     }
+    for(int i = 0; i < n; i++) cout << A[i] << " "; cout << endl;
 }
 
 signed main()
